f090fead66bf50384da787506823d2d7052a5ff9
anir-ab <anibhagwat17@gmail.com>
2023-05-31 23:29:58 +0530

    wokring queries and output

--- app.py
+++ app.py
@@ -1,7 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import mysql.connector
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="password",
+    database="explore"
+)
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+@app.route('/')
+def index():
+    cursor = db.cursor()
+    cursor.execute("SHOW TABLES")
+    tables = cursor.fetchall()
+    return render_template('index.html', tables=tables)
+
+@app.route('/query', methods=['POST'])
+def query():
+    query = request.form['query']
+    cursor = db.cursor()
+    cursor.execute(query)
+    result = cursor.fetchall()
+
+    cursor.execute("SHOW TABLES")
+    tables = cursor.fetchall()  # Fetch tables again to pass to the template
+
+    return render_template('index.html', tables=tables, result=result, query=query)
+
+if __name__ == '__main__':
+    app.run(debug=True)
